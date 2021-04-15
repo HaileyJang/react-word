a10c72feac297bf37748652b96b9613cb81871e5
Hailey Jang <hailey.jang.01@gmail.com>
2021-04-15 15:57:59 -0700

    Fix the recharts data issue and alignment issue:

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,41 @@
 import React from 'react';
 import './App.css';
 import { Word } from './Word';
-import { Dropdown, Table } from 'react-bootstrap';
+import { Col, Container, Dropdown, DropdownButton, Row, Table } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.css';
-import { faHome } from "@fortawesome/free-solid-svg-icons";
+import { faCog } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 function App() {
+
+  let dropDownLinkStyle = {
+    backgroundColor: "white",
+    color: "black",
+  };
+
   return (
     <>
-    <div className="App">
-      <div className = "container">
-        <div className = "bigFont"><b>Words</b></div>
-        <div className = "push-right">
-    <Dropdown>
-  <Dropdown.Toggle className = "customDropdown" id="dropdown-basic">
-    Today
-  </Dropdown.Toggle>
-  </Dropdown>
-  <FontAwesomeIcon icon = {faHome}/>
-  </div>
-  </div>
-  <text className = "lastEdit"> Last Edit: 12:45 am</text>
-      <div className = "wordList">
-      <Table hover>
-        <Word wordNumber = {1} wordCount = {78} percent = {7}/>
-        <Word wordNumber = {2} wordCount = {100} percent = {3}/>
-        <Word wordNumber = {3} wordCount = {28} percent = {2}/>
-        <Word wordNumber = {4} wordCount = {84} percent = {-3}/>
-        <Word wordNumber = {5} wordCount = {14} percent = {-9}/>
-      </Table>
-      </div>
-    </div>
-    </>
+    <div className = "app">
+    <Container fluid><Row><Col sm = {10}>
+    <b>Words</b></Col>
+    <Col sm = {1}>
+      <Dropdown>
+          <DropdownButton style={dropDownLinkStyle} title="Today" size="sm">
+            Today
+  </DropdownButton>
+        </Dropdown></Col>
+        <Col sm = {1}><FontAwesomeIcon icon={faCog} size="lg"/></Col></Row>
+      </Container>
+      <text className="lastEdit"> Last Edit: 12:45 am</text>
+        <Table hover>
+          <Word wordNumber={1} wordCount={78} percent={7} data = {[{ name: "a", value: 30 }, { name: "b", value: 60 }, { name: "c", value: 90 }, { name: "d", value: 100 }]}/>
+          <Word wordNumber={2} wordCount={100} percent={3} data = {[{ name: "a", value: 30 }, { name: "b", value: 70 }, { name: "c", value: 90 }, { name: "d", value: 80 }]}/>
+          <Word wordNumber={3} wordCount={28} percent={2} data = {[{ name: "a", value: 30 }, { name: "b", value: 70 }, { name: "c", value: 80 }, { name: "d", value: 90 }]}/>
+          <Word wordNumber={4} wordCount={84} percent={-3} data = {[{ name: "a", value: 90 }, { name: "b", value: 70 }, { name: "c", value: 50 }, { name: "d", value: 40 }]}/>
+          <Word wordNumber={5} wordCount={14} percent={-9} data = {[{ name: "a", value: 90 }, { name: "b", value: 70 }, { name: "c", value: 50 }, { name: "d", value: 20 }]}/>
+        </Table>
+        </div>
+        </>
   );
 }
 
--- src/Word.tsx
+++ src/Word.tsx
@@ -1,61 +1,93 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faArrowCircleUp } from "@fortawesome/free-solid-svg-icons";
+import { faArrowCircleDown, faArrowCircleUp } from "@fortawesome/free-solid-svg-icons";
 import React from 'react';
-import { Area, AreaChart } from 'recharts';
+import { Area, AreaChart, ResponsiveContainer } from 'recharts';
 
 interface Props {
     wordNumber: number,
     wordCount: number,
     percent: number,
+    data : Array<Object>
 }
 
-export function Word(props: Props){
+export function Word(props: Props) {
 
-    const handleColorChange = () => {
-        if (props.percent > 0){
-            return "green"
+    let percentGreen = {
+        color: "green"
+    }
+
+    let percentRed = {
+        color: "red"
+    }
+
+    const handlePercentColorChange = () => {
+        if (props.percent > 0) {
+            return percentGreen
         } else {
-            return "Blue"
+            return percentRed
         }
     }
-    
-    const data = [
-        {name: "a", value: 3000}, {name: "b", value: 4000}, {name: "c", value: 3000}, {name: "d", value: 3000}
-      ];
+
+    const handlefillColorChange = () => {
+        if (props.percent > 0) {
+            return "url(#colorUv)" 
+        } else {
+            return "url(#colorPv)" 
+        }
+    }
+
+
+    const handleArrowColorChange = () => {
+        if (props.percent > 0) {
+            return <FontAwesomeIcon icon={faArrowCircleUp} color="green" size="lg" />
+        } else {
+            return <FontAwesomeIcon icon={faArrowCircleDown} color="red" size="lg" />
+        }
+    }
 
     return (
-    <>
-        <tbody>
-          <tr>
-            <td>
-                Word {props.wordNumber}
-            </td>
-            <td className = "bigFont">
-                <b>{props.wordCount}X</b>
-            </td>
-            <td color = {handleColorChange()}>
-                {props.percent}%
-            </td>
-            <td>
-            <AreaChart
-          width={100}
-          height={20}
-          data={data}
-          margin={{
-            top: 5,
-            right: 0,
-            left: 0,
-            bottom: 5,
-          }}>
-          <Area type="monotone" dataKey="value" stroke="#8884d8" fill="#8884d8" />
-          </AreaChart>
-            </td>
-            <td>
-                <FontAwesomeIcon icon = {faArrowCircleUp} color= "green" size = "lg"/>
-            </td>
-        </tr>
-        </tbody>
-    </>
+        <>
+            <tbody>
+                <tr>
+                    <td>
+                        Word {props.wordNumber}
+                    </td>
+                    <td className="bigFont">
+                        <b>{props.wordCount}X</b>
+                    </td>
+                    
+                    <td className = "subject"> 
+                        <AreaChart
+                            width={100}
+                            height={30}
+                            data={props.data}
+                            margin={{
+                                top: 5,
+                                right: 0,
+                                left: 0,
+                                bottom: 5,
+                            }}>
+                              <defs>
+    <linearGradient id="colorUv" x1="0" y1="0" x2="0" y2="1">
+      <stop offset="5%" stopColor="green" stopOpacity={0.8}/>
+      <stop offset="95%" stopColor="green" stopOpacity={0}/>
+    </linearGradient>
+    <linearGradient id="colorPv" x1="0" y1="0" x2="0" y2="1">
+      <stop offset="5%" stopColor="red" stopOpacity={0.8}/>
+      <stop offset="95%" stopColor="red" stopOpacity={0}/>
+    </linearGradient>
+  </defs>
+                            <Area type="monotone" dataKey="value" stroke="#8884d8" fill={handlefillColorChange()} />
+                        </AreaChart>
+                    </td><td style={handlePercentColorChange()}>
+                        <b>{props.percent}%</b>
+                    </td>
+                    <td>
+                        {handleArrowColorChange()}
+                    </td>
+                </tr>
+            </tbody>
+        </>
     )
-    
+
 }
